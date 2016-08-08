aa5e79e0ac75133c4a6b754a0584ec74d38a8aa9
Dieter Luypaert <dieterluypaert@gmail.com>
2016-08-08 18:42:06 +0200

    add zsh, autoenv

--- brew.sh
+++ brew.sh
@@ -28,7 +28,7 @@ brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-brew install bash
+brew install autoenv bash zsh
 brew tap homebrew/versions
 brew install bash-completion2
 
