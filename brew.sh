bf728d939bc4776dba9cfa1fb7b823c2cc40e44a
Dieter Luypaert <dieterluypaert@gmail.com>
2016-11-11 20:11:20 +0100

    no custom openssh

--- brew.sh
+++ brew.sh
@@ -38,7 +38,6 @@ brew install wget --with-iri
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
 brew install tree
 
 # Git bins
