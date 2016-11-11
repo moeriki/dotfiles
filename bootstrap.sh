da78d3203f8126e59c9f19d221402d05e95efe8a
Dieter Luypaert <dieterluypaert@gmail.com>
2016-11-11 17:10:11 +0100

    tabs to spaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,18 @@
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".gitmodules" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.zsh_profile;
+  rsync --exclude ".git/" --exclude ".gitmodules" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+  source ~/.zsh_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+  doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
 fi;
 unset doIt;
