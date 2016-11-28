40e5219a068a4816ee3b02b4571321ea6c890698
Dieter Luypaert <dieterluypaert@gmail.com>
2016-11-28 15:41:07 +0100

    updated hyper config

--- .hyper.js
+++ .hyper.js
@@ -1,17 +1,44 @@
 module.exports = {
   config: {
+    // default font size in pixels for all tabs
+    fontSize: 13,
+
+    // font family with optional fallbacks
+    fontFamily: '"Operator Mono for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: 'rgba(248,28,229,0.8)',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
+
+    // color of the text
+    foregroundColor: '#fff',
 
     // terminal background color
-    backgroundColor: '#3c4c55', // default: #000
-
-    // set to false for no bell
-    bell: 'SOUND',
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
+    backgroundColor: '#000',
 
     // border color (window, tabs)
     borderColor: '#333',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: '',
+
+    // set to `true` if you're using a Linux set up
+    // that doesn't shows native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '12px 14px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -35,33 +62,6 @@ module.exports = {
       lightWhite: '#ffffff'
     },
 
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
-
-    // custom css to embed in the main window
-    css: '',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
-
-    // for environment variables
-    env: {},
-
-    // font family with optional fallbacks
-    fontFamily: '"Operator Mono for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-
-    // default font size in pixels for all tabs
-    fontSize: 13,
-
-    // color of the text
-    foregroundColor: '#fff',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
     shell: '',
@@ -70,22 +70,20 @@ module.exports = {
     // by default ['--login'] will be used
     shellArgs: ['--login'],
 
-    // custom css to embed in the terminal window
-    termCSS: '',
+    // for environment variables
+    env: {},
 
-    transparentBg: {
-      opacity: '0.15',
-      WebkitFilter: 'blur(5px)',
-    },
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
 
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
-
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: [
-  ],
 
   // a list of plugins to fetch and install from npm
   // format: [@org/]project[#version]
@@ -104,4 +102,8 @@ module.exports = {
     // 'nova-hyperterm',
   ],
 
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
 };
