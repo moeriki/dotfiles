32c99b13181ef11154321c3c02d7ac9d13d31cdd
Dieter Luypaert <dieterluypaert@gmail.com>
2017-04-10 15:52:14 +0200

    add hyperterm-tabs for sweet reordering

--- .hyper.js
+++ .hyper.js
@@ -108,6 +108,7 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [
     'hyper-snazzy',
+    'hyperterm-tabs',
     'hyper-tabs-enhanced',
     'hypercwd',
     'hyperterm-alternatescroll',
